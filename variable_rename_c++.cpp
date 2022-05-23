3c7c12c7f02019761596a31d545e63b757d21b26
Sumin_Jin <89318430+SuminJin0211@users.noreply.github.com>
2022-05-23 01:55:33 -0400

    Add files via upload

---
+++ variable_rename_c++.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num = 3;
+    cout << "변수 num의 값은" << num << "입니다 .\n";
+    num=5;
+    cout << " The variable value has changed.\n";
+    cout << " New variable is  " << num ;
+    return 0;
+}
