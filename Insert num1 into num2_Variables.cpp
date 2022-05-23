b4c769448ed68e1d7e2eed0f9fd7d1f713dfa82d
Sumin_Jin <89318430+SuminJin0211@users.noreply.github.com>
2022-05-23 02:00:07 -0400

    Add files via upload

---
+++ Insert num1 into num2_Variables.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2 ;
+    num1 =3;
+    cout << " Num1 is " << num1;
+    num2 = num1;
+    cout << "\n Insert Num1 value into Num2";
+    cout << "\n Num2 is "<<num2;
+    return 0;
+}
